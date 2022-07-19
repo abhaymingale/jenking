b62ee372d8e9e07c00e02af936cbad6e381f7f2e
Abhay Ingale <abhaymingale@gmail.com>
2022-07-19 06:25:53 +0530

    First Comment
    Change-Id: I65a3f547c82b3a4b9a7fcc6e2580d4893e6849bf

---
+++ jenkingpractices/src/main/java/com/example/jenkingpractices/JenkingpracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkingpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkingpracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkingpracticesApplication.class, args);
+	}
+
+}
---
+++ jenkingpractices/src/test/java/com/example/jenkingpractices/JenkingpracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkingpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkingpracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
